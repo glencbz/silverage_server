dc0e329e9b6fc8436b66a6d925766a27d4f4b8db
Glen Choo <glencbz@gmail.com>
2016-07-31 18:18:25 -0400

    API-ed imaged recognition

--- img_post.js
+++ img_post.js
@@ -1,8 +1,7 @@
 var // express = require('express'),
-  // bodyParser = require('body-parser'),
   fs = require('fs'),
-  // busboy = require('connect-busboy'),
-  execSync = require('child_process').execSync;
+  spawnSync = require('child_process').spawnSync;
+
 
 var ext = '.jpg';
 
@@ -11,8 +10,8 @@ function fileName(){
 }
 
 function formatOutput(outputString){
+  console.log(outputString);
   var lines = outputString.split('\n');
-
   lines = lines.filter(function(s){
     return s.charAt(0) == 'I';
   });
@@ -20,37 +19,37 @@ function formatOutput(outputString){
     return s.substr(s.indexOf(']') + 1).split(':');
   });
   lines = lines.map(function(strs){
-    return [strs[0].substring(0, strs[0].indexOf('(')), parseFloat(rawStrs[1].trim())]
+    return [strs[0].substring(0, strs[0].indexOf('(')).trim(), parseFloat(strs[1].trim())]
   });
-
+  console.log("lines1", lines);
   var result = {};
   for(var i = 0; i < lines.length; i++)
-    result[lines[0]] = lines[1];
+    result[lines[i][0]] = lines[i][1];
 
   return result;
 }
 
-function writeToFile(filePath, callback){
-  fstream = fs.createWriteStream(filePath);
+function writeToFile(filePath, file, callback){
+  var fstream = fs.createWriteStream(filePath);
   file.pipe(fstream);
-  file.on('end', callback());
+  file.on('end', callback);
 }
 
 function regImg(filePath){
   console.log('begin recognition');
-  var rawResult = execSync('/home/glen/silverage/tensorflow/tensorflow/bazel-bin/tensorflow/examples/label_image/label_image', ['--graph=/home/glen/silverage/tensorflow/tensorflow/class_w10/graph.pb', '--labels=/home/glen/silverage/tensorflow/tensorflow/class_w10/labels.txt', '--output_layer=final_result','--image=' + filePath]);
+  console.log(filePath);
+  var rawResult = spawnSync('/home/glen/silverage/tensorflow/tensorflow/bazel-bin/tensorflow/examples/label_image/label_image', ['--graph=/home/glen/silverage/tensorflow/tensorflow/class_w10/graph.pb', '--labels=/home/glen/silverage/tensorflow/tensorflow/class_w10/labels.txt', '--output_layer=final_result', '--image=' + filePath]).stderr.toString();
   console.log('recognition done');
-  return formatOutput(imgOutput);
+  return formatOutput(rawResult);
 }
 
-export default function(app){
+module.exports=function(app){
 	app.post('/upload', function(req, res){
 		console.log('req received');
-		var fstream;
 		req.pipe(req.busboy);
 		req.busboy.on('file', function(fieldname, file){
       var filePath = fileName();
-      writeToFile(filePath, function(){
+      writeToFile(filePath, file, function(){
         var result = regImg(filePath);
         res.send(JSON.stringify(result));
       });
